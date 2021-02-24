e30f9241825396d953055eb18eba5e2a1c4370ee
Roland <z5310743@ad.unsw.edu.au>
2021-02-24 11:09:21 +1100

    starting test file

---
+++ tests/channel_test.py
@@ -0,0 +1,13 @@
+#Impoet pytest
+import pytest 
+
+#Import functions from channel for testing
+from src.channel import channel_invite_v1
+from src.channel import channel_details_v1
+from src.channel import channel_messages_v1
+from src.channel import channel_join_v1
+from src.channel import channel_leave_v1
+from src.channel import channel_addowner_v1
+from src.channel import channel_removeowner_v1
+
+
