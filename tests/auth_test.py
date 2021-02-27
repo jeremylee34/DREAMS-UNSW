aa892c39fdcb3bee2ab3f131a9d0b023d129a0a0
Sing <fanthom1234@gmail.com>
2021-02-28 02:04:23 +0700

    add more to test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -3,26 +3,35 @@ import pytest
 from src.auth import auth_login_v1
 from src.auth import auth_register_v1
 from src.error import InputError
-from tests.auth_info import users
 
 #tests for auth_login
 def test_login_invalid_email():
-    pass
+    with pytest.raises(Exception):
+        auth_login_v1("fafasf@gmail.com", "asdf1234@") == "user_1"
+    with pytest.raises(Exception):
+        auth_login_v1("asd@gmail.com", "asdfasf1234@") == "user_2"
+    with pytest.raises(Exception):
+        auth_login_v1("2424ssss@gmail.com", "asasfdf1234@") == "user_3"
+    with pytest.raises(Exception):
+        auth_login_v1("42542552@gmail.com", "asdasff1234@") == "user_4"
 
 def test_login_valid_email():
-    pass
+    assert ("asdfg.com", "a2sdf" == "invalid mail")
+    assert ("asdfg@gmailcom", "as2df" == "invalid mail")    
+    assert ("asdfgcom", "asdssf" == "invalid mail")    
+    assert ("asdfg@com", "asaadf" == "invalid mail")        
 
 def test_login_email_unshared():
     pass
 
-def test_login_email_shared():
-    pass
+#def test_login_email_shared():
+    #pass
 
 def test_login_incorrect_password():
     pass
 
-def test_login_correct_password():
-    pass
+#def test_login_correct_password():
+    #pass
 
 
 #test for auth_register
