b8b9ba102efbac4c3bc93e5fd19ecd4c42c84377
Sing <fanthom1234@gmail.com>
2021-03-04 22:44:14 +0700

    handle except unique string

--- src/auth.py
+++ src/auth.py
@@ -1,6 +1,6 @@
-from src.auth_info import users
+from auth_info import users
 import re
-from src.error import InputError
+from error import InputError
 
 def auth_login_v1(email, password):
     regex = '^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$'
@@ -44,11 +44,13 @@ def auth_register_v1(email, password, name_first, name_last):
     for i in users:
         count += 1
     users[f"user{count}"] = {}
+
     #checks for valid email
     if re.search(regex, email):
         pass
     else:    
         raise InputError("Invalid email")
+
     #checks for shared email
     if count == 1:
         users[f"user{count}"]["email"] = email
@@ -59,38 +61,68 @@ def auth_register_v1(email, password, name_first, name_last):
                 raise InputError("Email is already used")
             x += 1
         users[f"user{count}"]["email"] = email
+
     #checks for valid password
     if len(password) >= 6:
         users[f"user{count}"]["password"] = password
     else:
         raise InputError("Password too short")
+
     #checks for valid firstname
     if len(name_first) >= 1 and len(name_first) <= 50:
         users[f"user{count}"]["firstname"] = name_first
     else:
         raise InputError("Invalid firstname")
+
     #checks for valid lastname
     if len(name_last) >= 1 and len(name_last) <= 50:
         users[f"user{count}"]["Lastname"] = name_last
     else:
         raise InputError("Invalid lastname")
     print(count)
-    #handle	
-    handle = ''
-    #error will occur before getting here?
-    conca = users[f"user{count}"]["firstname"]+users[f"user{count}"]["lastname"]
-    r = re.compiler(r"[@]")
-    cut_ad = r.sub("",s)
-    handle += conca.replace(" ","")
-    if len(handle)>20
-	raise InputError("too long conca")    
+
+    ####################
+    ###__add handle__###	
+    ####################
+
+    #make lower case
+    handle = (name_first + name_last).lower()
+
+    #replace ' ' and '@' with ''
+    handle = handle.replace("@", "")
+    handle = handle.replace(" ", "")
+
+    #check 20 chars; if exceed cut'em
+    if len(handle) > 20:
+        handle = handle[:20]
+
+    #check if handle repeat itself 
+    ##last_idx = -1
+    ##num_add = 0
+
+    ##for x in users:
+    ##    if users[f"user{count}"]["handle"] == handle:
+    ##        if num_add >= 10:
+    ##            last_idx -= 1
+    ##        #change to list first 
+    ##        list_handle = list(handle)
+    ##        handle[last_idx] = num_add
+    ##        num_add += 1
+    ##        
+    ##    else:
+    ##        pass
+    
+    users[f"user{count}"]["handle"] = handle 
+    print(users)
 
     return {
         'auth_user_id' : count,
     }
 
-#auth_register_v1("honey@outlook.com", "hellooooo!!!", "Tim", "Oreo")
+auth_register_v1("honey@outlook.com", "hellooooo!!!", "12345@@67890", "123456  7890123")
+auth_register_v1("asdfy@outlook.com", "hasdfooooo!!!", "12345@@67890", "123456  7890s123")
 #auth_register_v1("tom@outlook.com", "hellooooo!!!", "Tom", "bite")
 #auth_register_v1("andy@gmail.com", "hellooooo!!!", "Andy", "cook")
 #auth_login_v1("andy@gmail.com", "hellooooo!!!")
 #auth_login_v1("tom@outlook.com", "hellooooo!!!")
+#print(users)
