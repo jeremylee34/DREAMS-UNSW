68939ea098e89c7ee15fadd540ea89a98bf4c9e1
Gordon <z5310742@student.unsw.edu.au>
2021-03-05 21:27:37 +1100

    Implemented channels_create_v1

--- src/channels.py
+++ src/channels.py
@@ -1,4 +1,8 @@
+from src.error import InputError
+from data import data
+
 def channels_list_v1(auth_user_id):
+
     return {
         'channels': [
         	{
@@ -19,6 +23,17 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
+    if len(name) > 20:
+        raise InputError('There is more than 20 characters in your channel name')
+    new_channel = {
+        "name": name,
+        "is_public": is_public,
+        "id": len(data['channels']),
+        "owner_members": [auth_user_id,],
+        "all_members": [],
+    }
+    id = len(data['channels'])
+    data['channels'].append(new_channel)
     return {
-        'channel_id': 1,
+        'channel_id': id,
     }
