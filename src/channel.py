4a169f1330579685756bbf06c6090a336de8afbb
Roland <z5310743@ad.unsw.edu.au>
2021-03-06 21:23:18 +1100

    implemnt invite

--- src/channel.py
+++ src/channel.py
@@ -3,9 +3,32 @@ from src.error import AccessError
 from src.data import data
 
 def channel_invite_v1(auth_user_id, channel_id, u_id):
+    errorcount = 0
+    #Checks if auth is a member of channel
+    #Raises AccessError if not
+    for channel in data['channels']
+        membersall = channel.get('all_members']
+        for member in membersall:
+            if member == auth_user_id:
+                errorcount = errorcount + 1
+            if errorcount = 0:
+                raise AccessError('the authorised user is not already a member of the channel')
+    #Loop through users list to check if u_id is valid
+    errorcount = 0
+    for users in data['users']:
+        if users['id'] == u_id:
+            errorcount = errorcount + 1
+    if errorcount == 0:
+        raise InputError('u_id does not refer to a valid user')                            
+    #Loop through channels list to check if channel_id is valid 
+    #If found, append the u_id to all_members in the channel 
+    errorcount = 0
     for channel in data['channels']:
         if channels['channel_id'] == channel_id:
-            channels['all_members'].append(u_id)        
+            channels['all_members'].append(u_id)
+            errorcount = errorcount + 1   
+    if errorcount == 0:
+        raise InputError('channel_id does not refer to a valid channel')                                       
     return {}
 
 def channel_details_v1(auth_user_id, channel_id):
