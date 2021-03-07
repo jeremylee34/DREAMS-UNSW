349fa67823f7291a94b074b7929d5c73bd8ca40c
Jeremy <z5316983@ad.unsw.edu.au>
2021-03-07 17:30:24 +1100

    Created public_channel fixture in channel_test.py

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -19,12 +19,16 @@ def auth_user1():
     auth_id = auth_register_v1("j.lee34@gmail.com", "123456789", "Jeremy", "Lee" )
     return auth_id['auth_user_id']
 
+@pytest.fixture
+def public_channel(auth_user1):
+    new_channel = channels_create_v1(auth_user1, "Public channel", True)
+    return new_channel
+
 """
 Test adding to empty channel
 """
-def test_channel_join_v1_empty_channel(clear_data, auth_user1):      
-    new_channel = channels_create_v1(auth_user1, "Empty channel", True)
-    channel_id = new_channel['channel_id']
+def test_channel_join_v1_empty_channel(clear_data, auth_user1, public_channel):        
+    channel_id = public_channel['channel_id']
     channel_join_v1(auth_user1, channel_id)
     channel_dict = channel_details_v1(auth_user1, channel_id)
     assert channel_dict["all_members"][0]['u_id'] == auth_user1
@@ -39,10 +43,9 @@ def test_channel_join_v1_access_error(clear_data, auth_user1):
         assert channel_join_v1(auth_user1, channel_id)
 
 """
-InputError to be thrown when channel is private
+InputError to be thrown when channel_id is invalid
 """
-def test_channel_join_v1_input_error(clear_data, auth_user1):
-    new_channel = channels_create_v1(auth_user1, "Public channel", True)
+def test_channel_join_v1_input_error(clear_data, auth_user1, public_channel):
     channel_id = 99999
     with pytest.raises(InputError):
         assert channel_join_v1(auth_user1, channel_id)
