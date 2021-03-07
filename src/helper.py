e9f6b5e3858681e040192d0544d1e474ecd7b63d
Jeremy <z5316983@ad.unsw.edu.au>
2021-03-07 17:51:46 +1100

    added check_user_in_channel function

--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,7 @@
 from src.error import InputError
 from src.error import AccessError
 
-def check_valid_channel(channel_id, data):
+def check_valid_channel(data, channel_id):
     valid_channel = False
     for channels in data['channels']:
         if channels['channel_id'] == channel_id:
@@ -9,6 +9,14 @@ def check_valid_channel(channel_id, data):
     if valid_channel is False:
         return False
 
-def check_public_channel(channel_id, data):
+def check_public_channel(data, channel_id):
     if data['channels'][channel_id]['is_public'] is False:
         return False
+
+def check_user_in_channel(data, channel_id, auth_user_id):
+    user_in_channel = False
+    for member in data['channels'][channel_id]['all_members']:
+        ## if auth_user_id matches the member
+        if member['u_id'] == auth_user_id:
+            user_in_channel = True
+    return user_in_channel
