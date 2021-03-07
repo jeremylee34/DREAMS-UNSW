5d5b1c5e3af3367d19dc2d3f53089f838b116d50
Jeremy <z5316983@ad.unsw.edu.au>
2021-03-07 13:28:43 +1100

    First iteration, not finished

--- src/channel.py
+++ src/channel.py
@@ -40,6 +40,28 @@ def channel_leave_v1(auth_user_id, channel_id):
     }
 
 def channel_join_v1(auth_user_id, channel_id):
+
+    if data['channels'][channel_id]['is_public'] == False:
+        raise AccessError
+    
+    user_in_channel = False
+    ## search thru all members
+    for i in range(len(data['channels'][channel_id]['all_members'])):
+        ## if auth_user_id matches the member
+        if data['channels'][channel_id]['all_members'][i]['u_id'] == auth_user_id:
+            user_in_channel = True
+
+    ## if user not added, 
+    if user_in_channel is False:
+        user = user_profile_v1(auth_user_id, auth_user_id)
+        user_to_append = {
+            'u_id' : auth_user_id,
+            'name_first' : user['name_first'],
+            'name_last' : user['name_last'],
+        }
+        data['channels'][channel_id]['all_members'].append(user)
+    else:
+        pass
     return {
     }
 
