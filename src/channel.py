c97354553109dcdd4153dcde256346efd38321dc
Jeremy <z5316983@ad.unsw.edu.au>
2021-04-01 17:08:52 +1100

    Pylinted channel.py

--- src/channel.py
+++ src/channel.py
@@ -10,31 +10,36 @@ from src.helper import check_valid_channel
 from src.helper import check_public_channel
 from src.helper import check_user_in_channel
 
-"""
-<This function adds the user with u_id to the channel with channel_id owned by auth_user_id>
 
-Arguments:
-    <auth_user_id> (<integer>)    - <unique id used to identify a particular user, member of the channel in this case>
-    <channel_id> (<integer>)    - <unique id used to identify the particular channel the u_id is being added to>
-    <u_id> (<integer>)    - <unique id used to identify the user who is being added to the channel>
-    
-Exceptions:
-    InputError - Occurs when channel_id does not refer to a valid channel or when u_id does not refer to a valid user
-    AccessError - Occurs when the user with input auth_user_id is not in the channel with input channel_id
-    
-Return Value:
-    Returns <{}> on u_id being succesfully added to channel with channel_id
-"""
+def channel_invite_v1(auth_user_id, channel_id, u_id):
+    """
+    <This function adds the user with u_id to the channel with channel_id owned by auth_user_id>
 
-def channel_invite_v1(auth_user_id, channel_id, u_id): 
-    #Loop through channels list to check if channel_id is valid 
+    Arguments:
+        <auth_user_id> (<integer>)    - <unique id used to identify a particular
+        user, member of the channel in this case>
+        <channel_id> (<integer>)    - <unique id used to identify the particular
+        channel the u_id is being added to>
+        <u_id> (<integer>)    - <unique id used to identify the user who is being
+        added to the channel>
+
+    Exceptions:
+        InputError - Occurs when channel_id does not refer to a valid channel or
+        when u_id does not refer to a valid user
+        AccessError - Occurs when the user with input auth_user_id is not in the
+        channel with input channel_id
+
+    Return Value:
+        Returns <{}> on u_id being succesfully added to channel with channel_id
+    """
+    #Loop through channels list to check if channel_id is valid
     #raises InputError if not
     errorcount = 0
     for channel in data['channels']:
         if channel['channel_id'] == channel_id:
             errorcount = errorcount + 1
     if errorcount == 0:
-        raise InputError('channel_id does not refer to a valid channel')    
+        raise InputError('channel_id does not refer to a valid channel')
     #Loop through users list to check if u_id is valid
     #raises InputError if not
     errorcount = 0
@@ -42,19 +47,19 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
         if users['id'] == u_id:
             errorcount = errorcount + 1
     if errorcount == 0:
-        raise InputError('u_id does not refer to a valid user')    
+        raise InputError('u_id does not refer to a valid user')
     errorcount = 0
     #Checks if auth is a member of channel
     #Raises AccessError if not
     for channel in data['channels']:
-        if channel['channel_id'] == channel_id:            
+        if channel['channel_id'] == channel_id:
             for idcheck in channel['all_members']:
                 if idcheck['u_id'] == auth_user_id:
                     errorcount = errorcount + 1
     if errorcount == 0:
-        raise AccessError('the authorised user is not already a member of the channel')                        
-                                          
-    #Append new_member dictionary to 'all_members' in channel    
+        raise AccessError('the authorised user is not already a member of the channel')
+
+    #Append new_member dictionary to 'all_members' in channel
     new_member = {
         "u_id": u_id,
         "name_first": data['users'][u_id]['firstname'],
@@ -62,28 +67,34 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
     }
     for channel in data['channels']:
         if channel['channel_id'] == channel_id:
-            channel['all_members'].append(new_member)    
+            channel['all_members'].append(new_member)
     return {}
-               
-"""
-<This function returns details of the channel with channel_id that the user with auth_user_id is part of>
 
-Arguments:
-    <auth_user_id> (<integer>)    - <unique id used to identify a particular user, member of the channel in this case>
-    <channel_id> (<integer>)    - <unique id used to identify the particular channel that the auth_user_id is retrieving
-                                    details on> 
-    
-Exceptions:
-    InputError - Occurs when channel_id does not refer to a valid channel 
-    AccessError - Occurs when the user with input auth_user_id is not a member of the channel with input channel_id
-    
-Return Value:
-    Returns <name> on auth_user_id being part of channel with channel_id and channel_id being valid
-    Returns <owner_members> on auth_user_id being part of channel with channel_id and channel_id being valid
-    Returns <all_members> on auth_user_id being part of channel with channel_id and channel_id being valid
-"""               
 
 def channel_details_v1(auth_user_id, channel_id):
+    """
+    <This function returns details of the channel with channel_id that the user
+    with auth_user_id is part of>
+
+    Arguments:
+        <auth_user_id> (<integer>)    - <unique id used to identify a particular
+        user, member of the channel in this case>
+        <channel_id> (<integer>)    - <unique id used to identify the particular
+        channel that the auth_user_id is retrieving details on>
+
+    Exceptions:
+        InputError - Occurs when channel_id does not refer to a valid channel
+        AccessError - Occurs when the user with input auth_user_id is not a
+        member of the channel with input channel_id
+
+    Return Value:
+        Returns <name> on auth_user_id being part of channel with channel_id
+        and channel_id being valid
+        Returns <owner_members> on auth_user_id being part of channel with
+        channel_id and channel_id being valid
+        Returns <all_members> on auth_user_id being part of channel with
+        channel_id and channel_id being valid
+    """
     #Loop through channels list to check if channel_id is valid
     #raises InputError if not
     errorcount = 0
@@ -93,7 +104,7 @@ def channel_details_v1(auth_user_id, channel_id):
     if errorcount == 0:
         raise InputError('Channel ID is not a valid channel')
     errorcount = 0
-    #Check if auth is part of channel     
+    #Check if auth is part of channel
     #Raises AccessError if not
     for channel in data['channels']:
         if channel['channel_id'] == channel_id:
@@ -101,8 +112,8 @@ def channel_details_v1(auth_user_id, channel_id):
                 if member['u_id'] == auth_user_id:
                     errorcount = errorcount + 1
     if errorcount == 0:
-        raise AccessError('Authorised user is not a member of channel with channel_id')      
-    #Make dictionary with required keys that is to be returned 
+        raise AccessError('Authorised user is not a member of channel with channel_id')
+    #Make dictionary with required keys that is to be returned
     channel_info = {
         "name": "",
         "owner_members": [],
@@ -115,31 +126,31 @@ def channel_details_v1(auth_user_id, channel_id):
             for owners in channel['owner_members']:
                 channel_info['owner_members'].append(owners)
             for members in channel['all_members']:
-                channel_info['all_members'].append(members)        
+                channel_info['all_members'].append(members)
     return channel_info
 
-"""
-Given a channel_id that an authorised user is in, return up to 50 messages after
-and including the "start" index. R
 
-Arguments:
-    auth_user_id (dict)    - unique id attributed to a particular member
-    channel_id (dict)    - unique id attributeed to a particular channel
-    start (int) - what index the message capture should start at
-
-Exceptions:
-    InputError  - Occurs when channel_id is not a valid channel
-    InputError - Occurs when start is greater than total number of messages in channel
-    AccessError - Occurs when authorised user is not a member of channel with channel_id
-
-Return Value:
-    Returns a list of message dictionaries, a start index and an end index (start + 50) 
-    if not yet at end of all messages in channel
-    Returns a list of message dictionaries, a start index and an end index (-1) 
-    if reached end of all messages in channel
-
-"""
 def channel_messages_v1(auth_user_id, channel_id, start):
+    """
+    Given a channel_id that an authorised user is in, return up to 50 messages after
+    and including the "start" index.
+
+    Arguments:
+        auth_user_id (dict)    - unique id attributed to a particular member
+        channel_id (dict)    - unique id attributeed to a particular channel
+        start (int) - what index the message capture should start at
+
+    Exceptions:
+        InputError  - Occurs when channel_id is not a valid channel
+        InputError - Occurs when start is greater than total number of messages in channel
+        AccessError - Occurs when authorised user is not a member of channel with channel_id
+
+    Return Value:
+        Returns a list of message dictionaries, a start index and an end index (start + 50)
+        if not yet at end of all messages in channel
+        Returns a list of message dictionaries, a start index and an end index (-1)
+        if reached end of all messages in channel
+    """
     # Check valid channel
     if check_valid_channel(data, channel_id) is False:
         raise InputError("Channel ID is not a valid channel")
@@ -147,11 +158,11 @@ def channel_messages_v1(auth_user_id, channel_id, start):
     # Check if start is greater than number of messages in channel
     if start > len(data['channels'][channel_id]['messages']):
         raise InputError("Start is greater than the total number of messages in the channel")
-    
+
     # Check if user is in channel
     if check_user_in_channel(data, channel_id, auth_user_id) is False:
         raise AccessError("Authorised user is not a member of channel with channel_id")
-    
+
     messages = []
     message_index = start
     message_index_end = start + 50
@@ -167,7 +178,7 @@ def channel_messages_v1(auth_user_id, channel_id, start):
         message_index += 1
         if message_index == message_index_end:
             break
-    
+
     # if reached end of messages before capturing 50 messages, set to -1
     if message_index != message_index_end:
         message_index_end = -1
@@ -179,25 +190,28 @@ def channel_messages_v1(auth_user_id, channel_id, start):
     }
 
 def channel_leave_v1(auth_user_id, channel_id):
+    """
+    asdasd
+    """
     return {
     }
 
-"""
-Given a channel_id of a channel that the authorised user can join, adds them to that channel
 
-Arguments:
-    auth_user_id (dict)    - unique id attributed to a particular member
-    channel_id (dict)    - unique id attributeed to a particular channel
-
-Exceptions:
-    InputError  - Occurs when channel_id is not a valid channel
-    AccessError - Occurs when channel_id refers to a channel that is private
-
-Return Value:
-    Returns nothing on all cases
-
-"""
 def channel_join_v1(auth_user_id, channel_id):
+    """
+    Given a channel_id of a channel that the authorised user can join, adds them to that channel
+
+    Arguments:
+        auth_user_id (dict)    - unique id attributed to a particular member
+        channel_id (dict)    - unique id attributeed to a particular channel
+
+    Exceptions:
+        InputError  - Occurs when channel_id is not a valid channel
+        AccessError - Occurs when channel_id refers to a channel that is private
+
+    Return Value:
+        Returns nothing on all cases
+    """
     # Check valid channel
     if check_valid_channel(data, channel_id) is False:
         raise InputError("Channel ID is not a valid channel")
@@ -222,9 +236,15 @@ def channel_join_v1(auth_user_id, channel_id):
     }
 
 def channel_addowner_v1(auth_user_id, channel_id, u_id):
+    """
+    asdasd
+    """
     return {
     }
 
 def channel_removeowner_v1(auth_user_id, channel_id, u_id):
+    """
+    asdasd
+    """
     return {
     }
