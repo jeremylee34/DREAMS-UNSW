7f10f92729e8f1b4ffd5996d6d3bdefd8b8f6153
Gordon <z5310742@student.unsw.edu.au>
2021-04-04 17:23:50 +1000

    Made messages be in the correct order, fixed remove to actually remove and fixed edit to edit the correct message

--- src/message.py
+++ src/message.py
@@ -29,6 +29,7 @@ def message_send_v1(token, channel_id, message):
     if joined != 1:
         raise AccessError('User has not joined the channel')
     message_id = len(data['message_ids'])
+    data['message_ids'].append(message_id)
     current_time = datetime.now()
     timestamp = round(current_time.replace(tzinfo=timezone.utc).timestamp(), 1)
     new_message = {
@@ -37,13 +38,12 @@ def message_send_v1(token, channel_id, message):
         'message': message,
         'time_created': timestamp
     }
-    data['channels'][channel_id]['messages'].append(new_message)
+    data['channels'][channel_id]['messages'].insert(0, new_message)
     return {
         'message_id': message_id,
     }
 
 def message_remove_v1(token, message_id):
-    exists = 0
     valid = 0
     validuser = 0
     payload = jwt.decode(token, SECRET, algorithms=['HS256'])
@@ -58,24 +58,26 @@ def message_remove_v1(token, message_id):
     for channel in data['channels']:
         for message in channel['messages']:
             if message['message_id'] == message_id:
+                if message['message'] == '':
+                    raise InputError('Message does not exist')
                 u_id = message['u_id']
                 current_channel = channel
                 channel_id = channel['channel_id']
-                exists = 1
-    if exists != 1:
-        raise InputError('Message does not exist')
-    for member in current_channel['owner_members']:
-        if member['u_id'] == auth_user_id and auth_user_id == u_id:
-            validuser = 1
+                for member in current_channel['owner_members']:
+                    if member['u_id'] == auth_user_id and auth_user_id == u_id:
+                        validuser = 1
     if validuser != 1:
         raise AccessError('Message was not sent by user and user is not an owner of the channel')
-    data['channels'][channel_id]['messages'][message_id]['message'] = ''
+    for message2 in data['channels'][channel_id]['messages']:
+        if message2['message_id'] == message_id:
+            message2['message'] = ''
     # messages = [i for i in current_channel['messages'] if not (i['message_id'] == auth_user)]
     # data['channels'][channel_id]['messages'] = messages
     return {
     }
 
 def message_edit_v1(token, message_id, message):
+    validuser = 0
     if len(message) > 1000:
         raise InputError('Message is more than 1000 characters')
     payload = jwt.decode(token, SECRET, algorithms=['HS256'])
@@ -88,19 +90,21 @@ def message_edit_v1(token, message_id, message):
     if valid != 1:
         raise AccessError('User does not exist')
     for channel in data['channels']:
-        for message in channel['messages']:
-            if message['message_id'] == message_id:
-                u_id = message['u_id']
+        for message2 in channel['messages']:
+            if message2['message_id'] == message_id:
+                u_id = message2['u_id']
                 current_channel = channel
-                channel_id = channel['channel_id']
-                if message['message'] == '':
+                if message2['message'] == '':
                     raise InputError('Message has been deleted')
     for member in current_channel['owner_members']:
         if member['u_id'] == auth_user_id and auth_user_id == u_id:
             validuser = 1
     if validuser != 1:
         raise AccessError('Message was not sent by user and user is not an owner of the channel')
-    data['channels'][channel_id]['messages'][message_id]['message'] = message
+    for channel2 in data['channels']:
+        for message3 in channel2['messages']:
+            if message3['message_id'] == message_id:
+                message3['message'] = message
     return {
     }
 def message_share_v1(token, og_message_id, message, channel_id, dm_id):
@@ -132,6 +136,7 @@ def message_share_v1(token, og_message_id, message, channel_id, dm_id):
                 shared_message = message2['message']
     shared_message += message
     message_id = len(data['message_ids'])
+    data['message_ids'].append(message_id)
     current_time = datetime.now()
     timestamp = round(current_time.replace(tzinfo=timezone.utc).timestamp(), 1)
     new_message = {
@@ -140,7 +145,7 @@ def message_share_v1(token, og_message_id, message, channel_id, dm_id):
         'message': shared_message,
         'time_created': timestamp
     }
-    data['channels'][channel_id]['messages'].append(new_message)
+    data['channels'][channel_id]['messages'].insert(0, new_message)
     return {
         'shared_message_id': message_id
     }
@@ -163,6 +168,7 @@ def message_senddm_v1(token, dm_id, message):
     if exists != 1:
         raise AccessError('User is not part of DM')
     message_id = len(data['message_ids'])
+    data['message_ids'].append(message_id)
     current_time = datetime.now()
     timestamp = round(current_time.replace(tzinfo=timezone.utc).timestamp(), 1)
     new_message = {
@@ -171,7 +177,7 @@ def message_senddm_v1(token, dm_id, message):
         'message': message,
         'time_created': timestamp
     }
-    data['dms'][dm_id]['messages'].append(new_message)
+    data['dms'][dm_id]['messages'].insert(0, new_message)
     return {
         'message_id': message_id
     }
