af11f82e4e4a786d6f16eaa019940c51e177d5ed
Gordon <z5310742@student.unsw.edu.au>
2021-04-04 17:13:35 +1000

    Fixed up tests

--- tests/message_test.py
+++ tests/message_test.py
@@ -12,13 +12,15 @@ from src.message import message_senddm_v1
 from src.message import message_share_v1
 from src.error import InputError
 from src.error import AccessError
+from src.dm import dm_create_v1
+from src.dm import dm_messages_v1
 
 
 @pytest.fixture
 def clear():
     clear_v1()
 # Tests for message_send_v1
-def test_message_send(cleara):
+def test_message_send(clear):
     user = auth_register_v1("gordonl@gmail.com", "1234567", "Gordon", "Liang")
     channel = channels_create_v1(user['token'], "Channel1", True)
     message = message_send_v1(user['token'], channel['channel_id'], 'Hello')
@@ -39,20 +41,20 @@ def test_message_send_multiple(clear):
     user = auth_register_v1("gordonl@gmail.com", "1234567", "Gordon", "Liang")
     channel = channels_create_v1(user['token'], "Channel1", True)
     channel2 = channels_create_v1(user['token'], "Channel2", True)
-    message = message_send_v1(user['token'], channel['channel_id'], 'Hello')
-    message2 = message_send_v1(user['token'], channel2['channel_id'], 'Hello123')
-    messages = channel_messages_v1(user['token'], channel['channel_id'], message['message_id'])
-    messages2 = channel_messages_v1(user['token'], channel2['channel_id'], message2['message_id'])
+    message_send_v1(user['token'], channel['channel_id'], 'Hello')
+    message_send_v1(user['token'], channel2['channel_id'], 'Hello123')
+    messages = channel_messages_v1(user['token'], channel['channel_id'], 0)
+    messages2 = channel_messages_v1(user['token'], channel2['channel_id'], 0)
     assert messages['messages'][0]['message'] == 'Hello'
     assert messages2['messages'][0]['message'] == 'Hello123'
 def test_message_send_multiple_messages(clear):
     user = auth_register_v1("gordonl@gmail.com", "1234567", "Gordon", "Liang")
     channel = channels_create_v1(user['token'], "Channel1", True)
-    message = message_send_v1(user['token'], channel['channel_id'], 'Hello')
-    message2 = message_send_v1(user['token'], channel['channel_id'], 'Hello123')
-    message3 = message_send_v1(user['token'], channel['channel_id'], 'Goodbye')
-    message4 = message_send_v1(user['token'], channel['channel_id'], 'Goodbye123')
-    messages = channel_messages_v1(user['token'], channel['channel_id'], message4['message_id'])
+    message_send_v1(user['token'], channel['channel_id'], 'Hello')
+    message_send_v1(user['token'], channel['channel_id'], 'Hello123')
+    message_send_v1(user['token'], channel['channel_id'], 'Goodbye')
+    message_send_v1(user['token'], channel['channel_id'], 'Goodbye123')
+    messages = channel_messages_v1(user['token'], channel['channel_id'], 0)
     assert messages['messages'][3]['message'] == 'Hello'
     assert messages['messages'][2]['message'] == 'Hello123'
     assert messages['messages'][1]['message'] == 'Goodbye'
@@ -69,7 +71,7 @@ def test_message_edit(clear):
     channel_id = channels_create_v1(token['token'], "Channel1", True)
     message_id = message_send_v1(token['token'], channel_id['channel_id'], 'Hello')
     message_edit_v1(token['token'], message_id['message_id'], 'Goodbye')
-    messages = channel_messages_v1(token['token'], channel_id['channel_id'], message_id['message_id'])
+    messages = channel_messages_v1(token['token'], channel_id['channel_id'], 0)
     assert messages['messages'][0]['message'] == 'Goodbye'
 def test_message_edit_input_error(clear):
     token = auth_register_v1("gordonl@gmail.com", "1234567", "Gordon", "Liang")
@@ -81,7 +83,7 @@ def test_message_edit_input_error2(clear):
     token = auth_register_v1("gordonl@gmail.com", "1234567", "Gordon", "Liang")
     channel_id = channels_create_v1(token['token'], "Channel1", True)
     message_id = message_send_v1(token['token'], channel_id['channel_id'], 'Hello')
-    message_remove_v1(token['token'], message_id)
+    message_remove_v1(token['token'], message_id['message_id'])
     with pytest.raises(InputError):
         assert message_edit_v1(token['token'], message_id['message_id'], 'Goodbye')
 def test_message_edit_access_error(clear):
@@ -98,8 +100,8 @@ def test_message_remove(clear):
     channel_id = channels_create_v1(token['token'], "Channel1", True)
     message_id = message_send_v1(token['token'], channel_id['channel_id'], 'Hello')
     message_remove_v1(token['token'], message_id['message_id'])
-    messages = channel_messages_v1(token['token'], channel_id['channel_id'], message_id['message_id'])
-    assert messages['messages'][0] is None
+    messages = channel_messages_v1(token['token'], channel_id['channel_id'], 0)
+    assert messages['messages'][0]['message'] == ''
 def test_message_remove_input_error(clear):
     token = auth_register_v1("gordonl@gmail.com", "1234567", "Gordon", "Liang")
     channel_id = channels_create_v1(token['token'], "Channel1", True)
@@ -126,19 +128,19 @@ def test_message_remove_multiple(clear):
     message_remove_v1(user['token'], message2['message_id'])
     message_remove_v1(user['token'], message3['message_id'])
     message_remove_v1(user['token'], message4['message_id'])
-    messages = channel_messages_v1(user['token'], channel['channel_id'], message4['message_id'])
-    assert messages['message'][0]['message'] == ''
-    assert messages['message'][1]['message'] == ''
-    assert messages['message'][2]['message'] == ''
-    assert messages['message'][3]['message'] == ''
+    messages = channel_messages_v1(user['token'], channel['channel_id'], 0)
+    assert messages['messages'][0]['message'] == ''
+    assert messages['messages'][1]['message'] == ''
+    assert messages['messages'][2]['message'] == ''
+    assert messages['messages'][3]['message'] == ''
 # Tests for message_share_v1
 def test_message_share_channel(clear):
     token = auth_register_v1("gordonl@gmail.com", "1234567", "Gordon", "Liang")
     channel_id = channels_create_v1(token['token'], "Channel1", True)
     channel_id2 = channels_create_v1(token['token'], "Channel2", True)
     message_id = message_send_v1(token['token'], channel_id['channel_id'], 'Hello')
-    shared_message = message_share_v1(token['token'], message_id['message_id'], '', channel_id2['channel_id'], -1)
-    messages = channel_messages_v1(token['token'], channel_id2['channel_id'], shared_message['shared_message_id'])
+    message_share_v1(token['token'], message_id['message_id'], '', channel_id2['channel_id'], -1)
+    messages = channel_messages_v1(token['token'], channel_id2['channel_id'], 0)
     assert messages['messages'][0]['message'] == 'Hello'
 def test_message_share_access_error(clear):
     token = auth_register_v1("gordonl@gmail.com", "1234567", "Gordon", "Liang")
@@ -164,10 +166,10 @@ def test_message_share_multiple(clear):
     message = message_send_v1(user['token'], channel['channel_id'], 'Hello')
     message2 = message_send_v1(user['token'], channel['channel_id'], 'Hello2')
     message3 = message_send_v1(user['token'], channel['channel_id'], 'Hello3')
-    shared_message = message_share_v1(user['token'], message['message_id'], '', channel2, -1)
-    shared_message2 = message_share_v1(user['token'], message2['message_id'], '', channel2, -1)
-    shared_message3 = message_share_v1(user['token'], message3['message_id'], '', channel2, -1)
-    messages = channel_messages_v1(user['token'], channel2['channel_id'], shared_message3['shared_message_id'])
+    message_share_v1(user['token'], message['message_id'], '', channel2['channel_id'], -1)
+    message_share_v1(user['token'], message2['message_id'], '', channel2['channel_id'], -1)
+    message_share_v1(user['token'], message3['message_id'], '', channel2['channel_id'], -1)
+    messages = channel_messages_v1(user['token'], channel2['channel_id'], 0)
     assert messages['messages'][0]['message'] == 'Hello3'
     assert messages['messages'][1]['message'] == 'Hello2'
     assert messages['messages'][2]['message'] == 'Hello'
@@ -176,8 +178,8 @@ def test_message_share_message_addition(clear):
     channel = channels_create_v1(user['token'], "Channel1", True)
     channel2 = channels_create_v1(user['token'], "Channel2", True)
     message = message_send_v1(user['token'], channel['channel_id'], 'Hello')
-    shared_message = message_share_v1(user['token'], message['message_id'], 'Goodbye', channel2, -1)
-    messages = channel_messages_v1(user['token'], channel2['channel_id'], shared_message['shared_message_id'])
+    message_share_v1(user['token'], message['message_id'], 'Goodbye', channel2['channel_id'], -1)
+    messages = channel_messages_v1(user['token'], channel2['channel_id'], 0)
     assert messages['messages'][0]['message'] == 'HelloGoodbye'
 # Tests for message_senddm_v1
 def test_message_senddm_v1(clear):
@@ -192,22 +194,22 @@ def test_message_senddm_input_error(clear):
     user2 = auth_register_v1("roland@gmail.com", "1234567", "Roland", "Lin")
     dms = dm_create_v1(user['token'], [user2['auth_user_id']])
     with pytest.raises(InputError):
-        message = message_senddm_v1(user['token'], dms['dm_id'], 'HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello')
+        assert message_senddm_v1(user['token'], dms['dm_id'], 'HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello')
 def test_message_senddm_access_error(clear):
     user = auth_register_v1("gordonl@gmail.com", "1234567", "Gordon", "Liang")
     user2 = auth_register_v1("roland@gmail.com", "1234567", "Roland", "Lin")
     user3 = auth_register_v1("jeremy@gmail.com", "1234567", "Jeremy", "Lee")
     dms = dm_create_v1(user2['token'], [user3['auth_user_id']])
     with pytest.raises(AccessError):
-        message = message_senddm_v1(user['token'], dms['dm_id'], 'Hello')
+        assert message_senddm_v1(user['token'], dms['dm_id'], 'Hello')
 def test_message_senddm_multiple(clear):
     user = auth_register_v1("gordonl@gmail.com", "1234567", "Gordon", "Liang")
     user2 = auth_register_v1("roland@gmail.com", "1234567", "Roland", "Lin")
     dms = dm_create_v1(user['token'], [user2['auth_user_id']])
-    message = message_senddm_v1(user['token'], dms['dm_id'], 'Hello')
-    message2 = message_senddm_v1(user['token'], dms['dm_id'], 'Hello2')
-    message3 = message_senddm_v1(user['token'], dms['dm_id'], 'Hello3')
-    messages = dm_messages_v1(user['token'], dms['dm_id'], message3['message_id'])
+    message_senddm_v1(user['token'], dms['dm_id'], 'Hello')
+    message_senddm_v1(user['token'], dms['dm_id'], 'Hello2')
+    message_senddm_v1(user['token'], dms['dm_id'], 'Hello3')
+    messages = dm_messages_v1(user['token'], dms['dm_id'], 0)
     assert messages['messages'][0]['message'] == 'Hello3'
     assert messages['messages'][1]['message'] == 'Hello2'
     assert messages['messages'][2]['message'] == 'Hello'
