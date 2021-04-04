ef598aae9d222b2ee35b6b1aa40ec911b25d53eb
Jeremy <z5316983@ad.unsw.edu.au>
2021-04-05 04:08:47 +1000

    Finalised dm.py and dm_test.py

--- src/dm.py
+++ src/dm.py
@@ -155,3 +155,156 @@ def dm_remove_v1(token, dm_id):
     return {
     }
 
+def dm_invite_v1(token, dm_id, u_id):
+    """
+    <Function is called by user refered by token to add u_id to DM>
+    
+    Arguments:
+        <token> (<string>)  - encoded id that identifies user session
+        <dm_id> (<integer>) - Identifier for particular DM 
+        <u_id> (<integer>)  - Identifier for particular user 
+        
+    Exceptions:
+        InputError  - Occurs when dm_id does not refer to an exising dm and u_id
+                      does not refer to a valid user   
+        AccessError  - Occurs when authorised user is not a member of the DM       
+        
+    Return Value: {} 
+    """
+    #Check if dm_id refers to a valid dm, raise InputError if not
+    if check_valid_dm(dm_id) == False:
+        raise InputError("dm_id does not refer to an existing dm")
+    #Check if u_id refers to a valid user, raise InputError if not 
+    if check_valid_user(u_id) == False:
+        raise InputError("u_id does not refer to a valid user")
+    user = token_to_u_id(token)
+    #Check if token user is a member of the specified dm, raise AccessError if not
+    if check_user_in_dm(user, dm_id) == False:
+        raise AccessError("the authorised user is not a member of this DM")
+    #Look for corresponding dm through dm_id then append invited user to members list
+    #Also update the name of the dm by adding new handle and re-sorting 
+    for dm in data['dms']:
+        if dm['dm_id'] == dm_id:
+            for users in data['users']:
+                if users['u_id'] ==  u_id:
+                    s_id = users['session_ids'][0]
+                    s_token = jwt.encode({'session_id': s_id}, SECRET, algorithm='HS256')
+                    profile = user_profile_v1(s_token, u_id)
+                    dm['members'].append(profile)
+                    handlelist = dm['name'].split(", ")
+                    handlelist.append(users['handle_str'])
+                    break 
+            handlelist.sort()
+            handlelist_string = ', '.join(map(str, handlelist))
+            dm['name'] = handlelist_string                
+    return {}
+       
+       
+def dm_leave_v1(token, dm_id):
+    """
+    <Function is called by user refered by token to leave DM>
+    
+    Arguments:
+        <token> (<string>)  - encoded id that identifies user session
+        <dm_id> (<integer>) - Identifier for particular DM 
+        
+    Exceptions:
+        InputError  - Occurs when dm_id does not refer to an exising dm   
+        AccessError  - Occurs when authorised user is not a member of the DM       
+        
+    Return Value: {} 
+    """
+    #Check if dm_id refers to a valid dm, raise InputError if not
+    if check_valid_dm(dm_id) == False:
+        raise InputError("dm_id does not refer to an existing dm")
+    user = token_to_u_id(token)
+    #Check if token user is a member of the specified dm, raise AccessError if not
+    if check_user_in_dm(user, dm_id) == False:
+        raise AccessError("the authorised user is not a member of this DM")
+    #Look for corresponding dm through dm_id then remove token user from members list
+    #Also remove the token users handle from name 
+    for dm in data['dms']:
+        if dm['dm_id'] == dm_id:
+            for users in data['users']:
+                if users['u_id'] == user:
+                    s_id = users['session_ids'][0]
+                    s_token = jwt.encode({'session_id': s_id}, SECRET, algorithm='HS256')
+                    profile = user_profile_v1(s_token, user)
+                    dm['members'].remove(profile)
+                    stringU = users['handle_str']                   
+                    handlelist = dm['name'].split(", ")
+                    handlelist.remove(stringU)
+                    break
+            handlelist.sort()
+            handlelist_string = ', '.join(map(str, handlelist))
+            dm['name'] = handlelist_string        
+    return{}
+    
+    
+def dm_messages_v1(token, dm_id, start):
+    """
+    <Function is called by user to return up to 50 messages from DM>
+    
+    Arguments:
+        <token> (<string>)  - encoded id that identifies user session
+        <dm_id> (<integer>) - Identifier for particular DM 
+        <start> (<integer>)  - First index of messages to be returned
+        
+    Exceptions:
+        InputError  - Occurs when dm_id does not refer to an exising dm and start
+                      is greater than the total number of messages in DM  
+        AccessError  - Occurs when authorised user is not a member of the DM       
+        
+    Return Value: 
+        messages  - List of dictionaries descibing each message 
+        start  - First index of messages returned
+        end  - Final index of messages returned, usually start + 50 but will be 
+               set to -1 if start + 50 surpasses the bound of messages in DM 
+    """
+    #Check if dm_id refers to a valid dm, raise InputError if not
+    if check_valid_dm(dm_id) == False:
+        raise InputError("dm_id does not refer to an existing dm")
+    #Check if start is greater tham total messages in the DM
+    #Raise InputError if it is 
+    # for dm in data['dms']:
+    #     if dm['dm_id'] == dm_id:
+    #         if start > len(dm['messages']):
+    #             raise InputError("Start is greater tham total messages in the DM")
+    if start > (len(data['dms'][dm_id]['messages']) - 1):
+        raise InputError("Start is greater tham total messages in the DM")
+
+
+    user = token_to_u_id(token)
+    #Check if token user is a member of the specified dm, raise AccessError if not
+    if check_user_in_dm(user, dm_id) == False:
+        raise AccessError("the authorised user is not a member of this DM")
+    #Create empty list to append to and return 
+    #Index names used to iterate through and act as a boundry 
+    messages = []
+    message_start = start
+    message_end = start + 50
+    #Find particular dm that dm_id refers to 
+    #Record components of each message while appending to messages list
+    #End loop once start index matches end index or last message is reached
+    for dm in data['dms']:
+        if dm['dm_id'] == dm_id:
+            for msgs in dm['messages']:
+                tempmsg = {
+                    'message_id': msgs['message_id'],
+                    'u_id': msgs['u_id'],
+                    'message': msgs['message'],
+                    'time_created': msgs['time_created']
+                }
+                messages.append(tempmsg)
+                message_start = message_start + 1
+                if message_start == message_end:
+                    break
+    #Check to see if end index is within the total number of messages
+    #If not, set to -1 to indicate no more can be returned
+    if message_start < message_end:
+        message_end = -1
+    return{
+        'messages': messages,
+        'start': start,
+        'end': message_end        
+    }
--- tests/dm_test.py
+++ tests/dm_test.py
@@ -6,8 +6,14 @@ from src.dm import dm_details_v1
 from src.dm import dm_list_v1
 from src.dm import dm_create_v1
 from src.dm import dm_remove_v1
-from src.auth import auth_register_v2
-from src.user import user_profile_v2
+from src.dm import dm_invite_v1
+from src.dm import dm_leave_v1
+from src.dm import dm_messages_v1
+from src.dm import dm_create_v1
+
+from src.auth import auth_register_v1
+from src.user import user_profile_v1
+from src.message import message_senddm_v1
 
 #Import error from src
 from src.error import InputError
@@ -20,22 +26,22 @@ import src.data
 #Make fixtures
 @pytest.fixture
 def user_token1():
-    user_token1 = auth_register_v2("Godan@gmail.com", "password", "Godan", "Liang")
+    user_token1 = auth_register_v1("Godan@gmail.com", "password", "Godan", "Liang")
     return user_token1
     
 @pytest.fixture
-def user_token2():
-    user_token2 = auth_register_v2("Jeremy@gmail.com", "password", "Jeremy", "Lee")
+def user_token2(user_token1):
+    user_token2 = auth_register_v1("Jeremy@gmail.com", "password", "Jeremy", "Lee")
     return user_token2
     
 @pytest.fixture
-def user_token3():
-    user_token3 = auth_register_v2("Roland@gmail.com", "password", "Roland", "Lin")
+def user_token3(user_token1, user_token2):
+    user_token3 = auth_register_v1("Roland@gmail.com", "password", "Roland", "Lin")
     return user_token3
 
 @pytest.fixture
-def unadded_user_token():
-    user_token = auth_register_v2("Bolin@gmail.com", "password", "Bolin", "Ngo")
+def unadded_user_token(user_token1, user_token2, user_token3):
+    user_token = auth_register_v1("Bolin@gmail.com", "password", "Bolin", "Ngo")
     return user_token
 
 @pytest.fixture
@@ -73,11 +79,11 @@ def test_dm_create_v1_simple(clear_data, user_token1, user_token2, user_token3):
     u_ids = [user_token2['auth_user_id'], user_token3['auth_user_id']]  
     dm_1 = dm_create_v1(user_token1['token'], u_ids)
     assert dm_1['dm_id'] == 0
-    user_profile_dict1 = user_profile_v2(user_token1['token'], user_token1['auth_user_id'])
+    user_profile_dict1 = user_profile_v1(user_token1['token'], user_token1['auth_user_id'])
     handle1 = user_profile_dict1['handle_str']
-    user_profile_dict2 = user_profile_v2(user_token2['token'], user_token2['auth_user_id'])
+    user_profile_dict2 = user_profile_v1(user_token2['token'], user_token2['auth_user_id'])
     handle2 = user_profile_dict2['handle_str']
-    user_profile_dict3 = user_profile_v2(user_token3['token'], user_token3['auth_user_id'])
+    user_profile_dict3 = user_profile_v1(user_token3['token'], user_token3['auth_user_id'])
     handle3 = user_profile_dict3['handle_str']
     assert dm_1['dm_name'] == f"{handle1}, {handle2}, {handle3}"
 
@@ -87,7 +93,7 @@ def test_dm_create_v1_simple(clear_data, user_token1, user_token2, user_token3):
 #     """
 #     u_ids = [user_token2['auth_user_id'], user_token3['auth_user_id']]  
 #     dm_1 = dm_create_v1(user_token1['token'], u_ids)
-#     user_profile_dict1 = user_profile_v2(user_token1['token'], user_token1['auth_user_id'])
+#     user_profile_dict1 = user_profile_v1(user_token1['token'], user_token1['auth_user_id'])
 #     user_u_id = user_profile_dict1['u_id']
 #     assert dm_1['owner'] == user_u_id
     
@@ -110,11 +116,11 @@ def test_dm_details_v1_simple(clear_data, user_token1, user_token2, user_token3,
     Testing whether dm_details_v1 returns the correct name and members
     """
     dm_details = dm_details_v1(user_token1['token'], dm_1['dm_id'])
-    user_profile_dict1 = user_profile_v2(user_token1['token'], user_token1['auth_user_id'])
+    user_profile_dict1 = user_profile_v1(user_token1['token'], user_token1['auth_user_id'])
     handle1 = user_profile_dict1['handle_str']
-    user_profile_dict2 = user_profile_v2(user_token2['token'], user_token2['auth_user_id'])
+    user_profile_dict2 = user_profile_v1(user_token2['token'], user_token2['auth_user_id'])
     handle2 = user_profile_dict2['handle_str']
-    user_profile_dict3 = user_profile_v2(user_token3['token'], user_token3['auth_user_id'])
+    user_profile_dict3 = user_profile_v1(user_token3['token'], user_token3['auth_user_id'])
     handle3 = user_profile_dict3['handle_str']
     assert dm_details['name'] == f"{handle1}, {handle2}, {handle3}"
     assert user_profile_dict1 in dm_details['members']
@@ -163,3 +169,113 @@ def test_dm_remove_v1(clear_data, user_token1, dm_1):
     dm_remove_v1(user_token1['token'], dm_1['dm_id'])
     with pytest.raises(InputError):
         assert dm_details_v1(user_token1['token'], dm_id)
+
+################################################################################
+################################################################################
+################################################################################
+
+def test_dm_invite_v1_InputError_1(clear_data, user_token1, user_token2):
+    """
+    InputError happnes when dm_id does not refer to a existing DM
+    """
+    with pytest.raises(InputError):
+        assert dm_invite_v1(user_token1['token'], 69, user_token2['auth_user_id'])
+        
+def test_dm_invite_v1_InputError_2(clear_data, user_token1, dm_1):
+    """
+    InputError happens when u_id does not refer to a valid user
+    """
+    with pytest.raises(InputError):
+        assert dm_invite_v1(user_token1['token'], dm_1['dm_id'], 69)
+        
+def test_dm_invite_v1_AccessError(clear_data, unadded_user_token, dm_2, user_token3):
+    """
+    AccessError happens when authorised user is not already a member of the DM
+    """
+    with pytest.raises(AccessError):
+        assert dm_invite_v1(unadded_user_token['token'], dm_2['dm_id'], user_token3['auth_user_id'])
+        
+def test_dm_invite_v1_Invite1(clear_data, user_token1, dm_2, user_token3):
+    """
+    Test whether user will be invited and added to the specified DM
+    """
+    dm_invite_v1(user_token1['token'], dm_2['dm_id'], user_token3['auth_user_id'])
+    dm_details = dm_details_v1(user_token1['token'], dm_2['dm_id'])
+    assert dm_details['members'][-1]['u_id'] == user_token3['auth_user_id']
+    
+def test_dm_invite_v1_Invite2(clear_data, user_token1, dm_2, user_token3, unadded_user_token):
+    """
+    Test whether two users can be invited and added to the specified DM properly
+    """
+    dm_invite_v1(user_token1['token'], dm_2['dm_id'], user_token3['auth_user_id'])
+    dm_invite_v1(user_token1['token'], dm_2['dm_id'], unadded_user_token['auth_user_id'])
+    dm_details = dm_details_v1(user_token1['token'], dm_2['dm_id'])
+    assert dm_details['members'][-2]['u_id'] == user_token3['auth_user_id']
+    assert dm_details['members'][-1]['u_id'] == unadded_user_token['auth_user_id']
+    
+def test_dm_leave_v1_InputError(clear_data, user_token1):
+    """
+    InputError happens when dm_id does not refer to a existing DM
+    """
+    with pytest.raises(InputError):
+        assert dm_leave_v1(user_token1['token'], 69)
+        
+def test_dm_leave_v1_AccessError(clear_data, unadded_user_token, dm_2):
+    """
+    AccessError happens when authorised user is not a member of the DM
+    """
+    with pytest.raises(AccessError):
+        assert dm_leave_v1(unadded_user_token['token'], dm_2['dm_id'])
+        
+def test_dm_leave_v1_Leave1(clear_data, user_token1, user_token2, user_token3, dm_2):
+    """
+    Test whether a user can leave the specified DM properly and changes are made
+    If the most recent user has left properly, the -1 index of members will be of the previous user
+    """
+    dm_invite_v1(user_token1['token'], dm_2['dm_id'], user_token3['auth_user_id'])    
+    dm_leave_v1(user_token3['token'], dm_2['dm_id'])
+    dm_details = dm_details_v1(user_token1['token'], dm_2['dm_id'])    
+    assert dm_details['members'][-1]['u_id'] == user_token2['auth_user_id']
+    
+def test_dm_leave_v1_LeaveAll(clear_data, user_token1, user_token3, user_token2, dm_2):
+    """
+    Test whether multiple users can leave the specified DM properly
+    If all members of the DM leave, the members list for that DM should be empty
+    Therefore, calling dm_details_v1 for anyone should raise an AccessError as the user is no longer part of the DM
+    """
+    dm_invite_v1(user_token1['token'], dm_2['dm_id'], user_token3['auth_user_id'])
+    dm_leave_v1(user_token3['token'], dm_2['dm_id'])
+    dm_leave_v1(user_token2['token'], dm_2['dm_id'])
+    dm_leave_v1(user_token1['token'], dm_2['dm_id'])
+    with pytest.raises(AccessError):        
+        assert dm_details_v1(user_token1['token'], dm_2['dm_id'])
+    with pytest.raises(AccessError):        
+        assert dm_details_v1(user_token2['token'], dm_2['dm_id'])
+    with pytest.raises(AccessError):        
+        assert dm_details_v1(user_token3['token'], dm_2['dm_id'])
+        
+
+def test_dm_messages_v1_InputError1(clear_data, user_token1):
+    """
+    InputError happens when dm_id does not refer to a existing DM
+    """
+    with pytest.raises(InputError):
+        assert dm_messages_v1(user_token1['token'], 69, 0)
+        
+def test_dm_messages_v1_InputError2(clear_data, user_token1, dm_1):
+    """
+    InputError happens when start is greater than total number of messages in 
+    the channel
+    """
+    message_id = message_senddm_v1(user_token1['token'], dm_1['dm_id'], 'hi')
+    with pytest.raises(InputError):
+        assert dm_messages_v1(user_token1['token'], dm_1['dm_id'], 1)
+    
+        
+def test_dm_messages_v1_AccessError(clear_data, unadded_user_token, dm_1, user_token1):
+    """
+    AccessError happens when authorised user is not a member of the DM
+    """
+    message_id = message_senddm_v1(user_token1['token'], dm_1['dm_id'], 'hi')
+    with pytest.raises(AccessError):
+        assert dm_messages_v1(unadded_user_token['token'], dm_1['dm_id'], 0)
