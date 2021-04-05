08afce150c8f1f07b4018472e6885dab8df2000d
Sing <fanthom1234@gmail.com>
2021-04-05 20:58:38 +0700

    add test

--- http_tests/other_test.py
+++ http_tests/other_test.py
@@ -11,23 +11,71 @@ from src.error import InputError, AccessError
 def clear_data():
     requests.delete(config.url + 'clear/v1')
 
+#search test
 def test_search_v2(clear_data):
-    user1 = requests.post(config.url + 'auth/register/v2', json = {
-        'email': 'ska@gmail.com',
+    user = requests.post(config.url + 'auth/register/v2', json = {
+        'email': 'skadi@gmail.com',
         'password': '1234aaaaaa',
         'name_first': 'Tom',
         'name_last': 'Diaaa',
     })
     #join server
-    
-    #send message
+    user_pl = user.json()
 
-    payload = user1.json()
-    requests.get(config.url + 'search/v1', json = {
-        'token': 'skadi@gmail.com',
-        'query_str': '1234aaaaaa',
+    channel_info = requests.post(config.url + 'channels/create/v2', json = {
+        'token': user_pl['token'],
+        'name': 'Channel1',
+        'is_public': True,
+    })
+    channel_info = channel_info.json()
+
+    #send message
+    requests.post(config.url + 'message/send/v2', json = {
+        'token': user_pl['token'],
+        'channel_id': channel_info['channel_id'],
+        'message': 'Hello',
     })
 
+    assert requests.get(config.url + 'search/v1', json = {
+        'token': user_pl['token'],
+        'query_str':'Hello',
+    }).status_code == 400
+
+def test_search_v2_input_error(clear_data):
+    user = requests.post(config.url + 'auth/register/v2', json = {
+        'email': 'skadi@gmail.com',
+        'password': '1234aaaaaa',
+        'name_first': 'Sing',
+        'name_last': 'Diaaa',
+    })
+    user_pl = user.json()
+
+    channel_info = requests.post(config.url + 'channels/create/v2', json = {
+        'token': user_pl['token'],
+        'name': 'Channel1',
+        'is_public': True,
+    })
+    channel_info = channel_info.json()
+
+    requests.post(config.url + 'message/send/v2', json = {
+        'token': user_pl['token'],
+        'channel_id': channel_info['channel_id'],
+        'message': 'Hello',
+    })
+
+    assert requests.get(config.url + 'search/v1', json = {
+        'token': user_pl['token'],
+        'query_str':'Hello' * 1000,
+    }).status_code == InputError.code
+
+def test_search_v2_input_token(clear_data):
+    assert requests.get(config.url + 'search/v1', json = {
+        'token': 6,
+        'query_str':'Hello',
+    }).status_code == AccessError.code
+
+
+#notification test
 def test_notifications_get(clear_data):
     user1 = requests.post(config.url + 'auth/register/v2', json = {
         'email': 'ska@gmail.com',
