d1c7748ba78441704b9f3acc76c57c2c5626ca75
Sing <fanthom1234@gmail.com>
2021-04-05 14:43:43 +0700

    add admin.py and other.py

--- src/other.py
+++ src/other.py
@@ -1,44 +1,133 @@
+import requests
+import jwt
 from src.data import data
+from src.channel import channel_messages_v1
+from src.channels import channels_list_v1
+
+from src.error import InputError, AccessError
+
 def clear_v1():
+    for x in data["users"]:
+        x["session_ids"].clear()
     data['users'].clear()
     data['channels'].clear()
-    data['messages'].clear()
+    data['dms'].clear() 
+    data['message_ids'].clear() 
+    data['token_list'].clear()
+    return {}
 
-def search_v2(token, query_str):
-    input_token = request.args.get('token')
-    input_query_str = request.args.get('query_str')
+
+def search_v1(token, query_str):
+    """
+    Description of function:
+        Given query string, return collection of messages
+        in all channel/DMs that user joined that matched query
+    Parameters:
+        token (str)
+        query_str (str)
+    Exceptions:
+        InputError - if query_str is above 1000 characters
+    Returns:
+        dictionary of message_id, u_id, message, time_created 
+    """
+    for tokens in data['token_list']:
+        if tokens == token:
+            valid = 1
+    if valid != 1:
+        raise AccessError('User does not exist')
+
+    msg_list = []
+    token = jwt.decode(token, 'HELLO', algorithms=['HS256'])
+
+    if len(query_str) > 1000:
+        raise InputError("query_str is above 1000 characters")
+
+    #check if valid
+    for user in data['users']:
+       for session in user['session_ids']:
+           if session == token["session_id"]:
+                u_id = user['u_id']
+                #getting all the channel that user is in //data[channels], data[dms]
+    for channel in data['channels']:
+        for message in channel['messages']:
+            if query_str == message['message']:
+                msg_list.append(message)
+    for dm in data['dms']:
+        for dm_message in dm['messages']:
+            if query_str == dm_message['message']:
+                msg_list.append(dm_message)
+
     return {
-       'messages': [
-            {
-                'message_id': 1,
-                'u_id': 1,
-                'message': 'Hello world',
-                'time_created': 1582426789,
-            }
-        ],
-    } #when call if info blank make it removed suer
+       'messages': msg_list
+    } 
 
-def notifications_get(token):
+def notifications_get_v1(token):
+    """
+    Description of function:
+        Remove the user(u_id) from the Dreams. 
+        Must have authority to do so, being admin.
+    Parameters:
+        token (str)
+        u_id (int)
+    Exceptions:
+        InputError - if the u_id is not a valid user
+        AccessError - Authorised user isn't an owner
+    Returns:
+        Blank dictionary
+    """
+    for tokens in data['token_list']:
+        if tokens == token:
+            valid = 1
+    if valid != 1:
+        raise AccessError('User does not exist')
+
     valid = 0
-    input_token = request.args.get('token')
-    decoded_token = jwt.decode(input_token, 'HELLO', algorithms=['HS256'])
-    get_notification = {"notification" : []}
-    all_msg = []
-
-    #find that person with token
-    for x in data['users']:
-        for y in x['session_ids']:
-            if decoded_token["session_ids"] == y:
-                #some actionsss
-                #search all msg in channel/dm
-                #collect, print20
-                #change all funcs to other file and do apply func in server
-
-    #collect all message
-    for x in dict(reversed(list(noti_list.items()))): #newest noti first
-        if len(all_msg <= 20)
-            all_msg.append(x['notification_message'])
-
+    decoded_token = jwt.decode(token, 'HELLO', algorithms=['HS256'])
+    msg_list = []
+    notification_num = 0
+    #check if valid
+    for user in data['users']:
+       for session in user['session_ids']:
+           if session == decoded_token["session_id"]:
+                u_id = user['u_id']
+                handle = user['handle_str']
+    for notification in data['notifications']:
+        if notification_num == 20:
+            break
+        if notification['u_id'] == u_id:
+            if f'@{handle}' in notification['message']:
+                if notification['channel_id'] == -1:
+                    dm_name = data['dms'][notification['channel_id']]['name']
+                    new_dict = {
+                        'channel_id': notification['channel_id'],
+                        'dm_id': notification['dm_id'],
+                        'notification_message': f"{handle} tagged you in {dm_name}",
+                    }
+                else:
+                    channel_name = data['channels'][notification['channel_id']]['name']
+                    new_dict = {
+                        'channel_id': notification['channel_id'],
+                        'dm_id': notification['dm_id'],
+                        'notification_message': f"{handle} tagged you in {channel_name}",
+                    }
+                msg_list.append(new_dict)
+            if notification['message'] == '':
+                if notification['channel_id'] == -1:
+                    dm_name = data['dms'][notification['channel_id']]['name']
+                    new_dict = {
+                        'channel_id': notification['channel_id'],
+                        'dm_id': notification['dm_id'],
+                        'notification_message': f"{handle} added you to {dm_name}"
+                    }
+                else:
+                    channel_name = data['channels'][notification['channel_id']]['name']
+                    new_dict = {
+                        'channel_id': notification['channel_id'],
+                        'dm_id': notification['dm_id'],
+                        'notification_message': f"{handle} added you to {channel_name}"
+                    }
+                msg_list.append(new_dict)
+            notification_num += 1
     return {
-        get_notification["notification"]['notification_message']
+        'notifications': msg_list
     }
