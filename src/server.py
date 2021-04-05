9bed065a4305d72d4cf54d9f02f1322db5792113
Roland <z5310743@ad.unsw.edu.au>
2021-04-06 02:16:55 +1000

    routesdone

--- src/server.py
+++ src/server.py
@@ -4,6 +4,13 @@ from flask import Flask, request
 from flask_cors import CORS
 from src.error import InputError
 from src import config
+from src.channel import channel_invite_v1
+from src.channel import channel_details_v1
+from src.channel import channel_addowner_v1
+from src.channel import channel_removeowner_v1
+from src.dm import dm_invite_v1
+from src.dm import dm_leave_v1
+from src.dm import dm_messages_v1
 
 def defaultHandler(err):
     response = err.get_response()
@@ -31,6 +38,54 @@ def echo():
     return dumps({
         'data': data
     })
+"""
+Channel server routes
+"""
+@APP.route("channel/invite/v2", methods=['POST'])
+def invite_channel():
+    data = request.get_json()
+    channel_invite_v1(data['token'], data['channel_id'], data['u_id'])
+    return dumps({})
+    
+@APP.route("channel/details/v2", methods=['GET'])
+def details_channel():
+    data = request.get_json()
+    channel_details_v1(data['token'], data['channel_id'])
+    return dumps({name, is_public, owner_members, all_members})
+    
+@APP.route("channel/addowner/v1", methods=['POST'])
+def addowner_channel():
+    data = request.get_json()
+    channel_addowner_v1(data['token'], data['channel_id'], data['u_id'])
+    return dumps({})
+    
+@APP.route("channel/removeowner/v1", methods=['POST'])
+def removeowner_channel():
+    data = request.get_json()
+    channel_removeowner_v1(data['token'], data['channel_id'], data['u_id'])
+    return dumps({})
+
+"""
+Dm server routes
+"""
+
+@APP.route("dm/invite/v1", methods=['POST'])
+def invite_dm():
+    data = request.get_json()
+    dm_invite_v1(data['token'], data['dm_id'], data['u_id'])
+    return dumps({})
+    
+@APP.route("dm/leave/v1", methods=['POST'])
+def leave_dm():
+    data = request.get_json()
+    dm_leave_v1(data['token'], data['dm_id'])
+    return dumps({})
+    
+@APP.route("dm/messages/v1", methods=['GET'])
+def messages_dm():
+    data = request.get_json()
+    dm_messages_v1(data['token'], data['dm_id'], data['start'])
+    return dumps({messages, start, end})
 
 if __name__ == "__main__":
     APP.run(port=config.port) # Do not edit this port
