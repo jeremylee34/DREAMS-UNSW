1bfabd992989c9c537c0bc6e1c92ac14be36fb15
Roland <z5310743@ad.unsw.edu.au>
2021-04-06 05:50:21 +1000

    hhtp

--- http_tests/dm_http_test.py
+++ http_tests/dm_http_test.py
@@ -1,17 +1,246 @@
+
+################################################################################
+#########################         IMPORTS          #############################
+################################################################################
+
 import pytest
+import jwt
 import requests
-from src.config import port
 from src.config import url
 from src.other import clear_v1
 from src.error import InputError
 from src.error import AccessError
 
-INVALID_ID = 1000
+################################################################################
+#########################     GLOBAL VARIABLES     #############################
+################################################################################
+
+INPUT_ERROR = 400
+ACCESS_ERROR = 403
+INVALID_ID = 9999
+SECRET = "HELLO"
+INVALID_TOKEN = jwt.encode({"session_id": 9999}, SECRET, algorithm = "HS256")
+
+################################################################################
+#########################         FIXTURES         #############################
+################################################################################
 
 @pytest.fixture
 def clear():
     requests.delete(f"{url}/clear/v1")
 
+@pytest.fixture
+def user_token1():
+    info1 = requests.post(f"{url}/auth/register/v2", json={
+        'email': 'Godan@gmail.com',
+        'password': 'password',
+        'name_first': 'Godan',
+        'name_last': 'Liang'
+    }).json()
+    return info1
+
+@pytest.fixture
+def user_token2(user_token1):
+    info2 = requests.post(f"{url}/auth/register/v2", json={
+        'email': 'Jeremy@gmail.com',
+        'password': 'password',
+        'name_first': 'Jeremy',
+        'name_last': 'Lee'
+    }).json()
+    return info2
+
+@pytest.fixture
+def user_token3(user_token1, user_token2):
+    info3 = requests.post(f"{url}/auth/register/v2", json={
+        'email': 'Roland@gmail.com',
+        'password': 'password',
+        'name_first': 'Roland',
+        'name_last': 'Lin'
+    }).json()
+    return info3
+
+@pytest.fixture
+def unadded_user_token(user_token1, user_token2, user_token3):
+    user_toke = requests.post(f"{url}/auth/register/v2", json={
+        'email': 'Bolin@gmail.com',
+        'password': 'password',
+        'name_first': 'Bolin',
+        'name_last': 'Ngo'
+    }).json()
+    return user_toke
+
+@pytest.fixture
+def dm_1(user_token1, user_token2, user_token3):
+    u_ids = [user_token2['auth_user_id'], user_token3['auth_user_id']]
+    dm = requests.post(f"{url}/dm/create/v1", json={
+        'token': user_token1['token'],
+        'u_ids': u_ids
+    }).json()
+    return dm
+
+@pytest.fixture
+def dm_2(user_token1, user_token2):
+    u_ids = [user_token2['auth_user_id']]
+    dm = requests.post(f"{url}/dm/create/v1", json={
+        'token': user_token1['token'],
+        'u_ids': u_ids
+    }).json()
+    return dm
+
+################################################################################
+#########################      dm_details tests    #############################
+################################################################################
+
+def test_dm_details_v1_input_error(clear, user_token1):
+    """
+    InputError to be thrown when DM ID is not a valid DM
+    """
+    assert requests.get(f"{url}/dm/details/v1", json={
+        'token': user_token1['token'],
+        'dm_id': INVALID_ID
+    }).status_code == INPUT_ERROR
+
+def test_dm_details_v1_access_error(clear, unadded_user_token, dm_1):
+    """
+    AccessError to be thrown when authorised user is not a member of this DM with dm_id
+    """
+    assert requests.get(f"{url}/dm/details/v1", json={
+        'token': unadded_user_token['token'],
+        'dm_id': dm_1['dm_id']
+    }).status_code == ACCESS_ERROR
+
+def test_dm_details_v1_simple(clear, user_token1, user_token2, user_token3, dm_1):
+    """
+    Testing whether dm_details_v1 returns the correct name and members
+    """
+    dm_details = requests.get(f"{url}/dm/details/v1", json={
+        'token': user_token1['token'],
+        'dm_id': dm_1['dm_id']
+    }).json()
+    user_profile_dict1 = requests.get(f"{url}/user/profile/v2", json={
+        'token': user_token1['token'],
+        'u_id': user_token1['auth_user_id']
+    }).json()
+    handle1 = user_profile_dict1['handle_str']
+    user_profile_dict2 = requests.get(f"{url}/user/profile/v2", json={
+        'token': user_token2['token'],
+        'u_id': user_token2['auth_user_id']
+    }).json()
+    handle2 = user_profile_dict2['handle_str']
+    user_profile_dict3 = requests.get(f"{url}/user/profile/v2", json={
+        'token': user_token3['token'],
+        'u_id': user_token3['auth_user_id']
+    }).json()
+    handle3 = user_profile_dict3['handle_str']
+    assert dm_details['name'] == f"{handle1}, {handle2}, {handle3}"
+    assert user_profile_dict1 in dm_details['members']
+    assert user_profile_dict2 in dm_details['members']
+    assert user_profile_dict3 in dm_details['members']
+
+################################################################################
+#########################       dm_list tests      #############################
+################################################################################
+
+def test_dm_list_v1_simple(clear, user_token1, dm_1, dm_2):
+    """
+    Testing whether dm_list_v1 returns the list of DMs correctly
+    """
+    dms = requests.get(f"{url}/dm/list/v1", json={
+        'token': user_token1['token']
+    }).json()
+    expected_dm = {
+        'dms': [
+            {
+                'dm_id': 0,
+                'name': 'godanliang, jeremylee, rolandlin'
+            },
+            {
+                'dm_id': 1,
+                'name': 'godanliang, jeremylee'
+            }
+        ]
+    }
+    assert dms == expected_dm
+
+################################################################################
+#########################      dm_create tests     #############################
+################################################################################
+
+def test_dm_create_v1_input_error(clear, user_token1):
+    """
+    InputError to be thrown when u_id does not refer to valid user
+    """
+    assert requests.post(f"{url}/dm/create/v1", json={
+        'token': user_token1['token'],
+        'u_ids': [INVALID_ID]
+    }).status_code == INPUT_ERROR
+
+def test_dm_create_v1_simple(clear, user_token1, user_token2, user_token3):
+    """
+    Testing whether dm_create_v1 returns the correct dm_id and dm_name
+    """
+    u_ids = [user_token2['auth_user_id'], user_token3['auth_user_id']]  
+    dm_1 = requests.post(f"{url}/dm/create/v1", json={
+        'token': user_token1['token'],
+        'u_ids': u_ids
+    }).json()
+    assert dm_1['dm_id'] == 0
+    user_profile_dict1 = requests.get(f"{url}/user/profile/v2", json={
+        'token': user_token1['token'],
+        'u_id': user_token1['auth_user_id']
+    }).json()
+    handle1 = user_profile_dict1['handle_str']
+    user_profile_dict2 = requests.get(f"{url}/user/profile/v2", json={
+        'token': user_token2['token'],
+        'u_id': user_token2['auth_user_id']
+    }).json()
+    handle2 = user_profile_dict2['handle_str']
+    user_profile_dict3 = requests.get(f"{url}/user/profile/v2", json={
+        'token': user_token3['token'],
+        'u_id': user_token3['auth_user_id']
+    }).json()
+    handle3 = user_profile_dict3['handle_str']
+    assert dm_1['dm_name'] == f"{handle1}, {handle2}, {handle3}"
+
+################################################################################
+#########################      dm_remove tests     #############################
+################################################################################
+
+def test_dm_remove_v1_input_error(clear, user_token1):
+    """
+    InputError to be thrown when dm_id does not refer to a valid DM
+    """
+    assert requests.delete(f"{url}/dm/remove/v1", json={
+        'token': user_token1['token'],
+        'dm_id': INVALID_ID
+    }).status_code == INPUT_ERROR
+    
+def test_dm_remove_v1_access_error(clear, unadded_user_token, dm_1):
+    """
+    AccessError to be thrown when the user is not the original DM creator
+    """
+    assert requests.delete(f"{url}/dm/remove/v1", json={
+        'token': unadded_user_token['token'],
+        'dm_id': dm_1['dm_id']
+    }).status_code == ACCESS_ERROR
+
+def test_dm_remove_v1(clear, user_token1, dm_1):
+    """
+    Testing whether dm is removed. If dm is removed, dm_details_v1 should raise
+    InputError since dm is removed.
+    """
+    requests.delete(f"{url}/dm/remove/v1", json={
+        'token': user_token1['token'],
+        'dm_id': dm_1['dm_id']
+    })
+    assert requests.get(f"{url}/dm/details/v1", json={
+        'token': user_token1['token'],
+        'dm_id': dm_1['dm_id']
+    }).status_code == INPUT_ERROR
+
+################################################################################
+#########################      dm_invite tests     #############################
+################################################################################
 
 def test_dm_invite_v1_InputError1(clear):
     """
@@ -254,7 +483,9 @@ def test_dm_invite_v1_Invite2(clear):
     assert dm_details['members'][-2]['u_id'] == reg_info3['auth_user_id']
     assert dm_details['members'][-1]['u_id'] == reg_info4['auth_user_id']
 
-
+################################################################################
+#########################      dm_leave tests      #############################
+################################################################################
 
 def test_dm_leave_v1_InputError(clear):
     """
@@ -416,7 +647,9 @@ def test_dm_leave_v1_LeaveALL(clear):
         'dm_id': dm_2['dm_id']
     }).status_code == 403
     
-    
+################################################################################
+#########################     dm_messages tests    #############################
+################################################################################
     
 def test_dm_messages_v1_InputError1(clear):
     """
@@ -461,7 +694,7 @@ def test_dm_messages_v1_InputError2(clear):
     dm_2 = dm_2.json()
     message_id = requests.post(f"{url}/message/senddm/v1", json={
         'token': reg_info1['token'],
-        'dm_id': dm_2['dm_id']
+        'dm_id': dm_2['dm_id'],
         'message': 'hi'
     })
     message_id = message_id.json()
@@ -503,7 +736,7 @@ def test_dm_messages_v1_AccessError(clear):
     dm_2 = dm_2.json()
     message_id = requests.post(f"{url}/message/senddm/v1", json={
         'token': reg_info1['token'],
-        'dm_id': dm_2['dm_id']
+        'dm_id': dm_2['dm_id'],
         'message': 'hi'
     })
     message_id = message_id.json()
@@ -538,17 +771,17 @@ def test_dm_messages_v1_simple(clear):
     dm_2 = dm_2.json()
     message_id = requests.post(f"{url}/message/senddm/v1", json={
         'token': reg_info1['token'],
-        'dm_id': dm_2['dm_id']
+        'dm_id': dm_2['dm_id'],
         'message': 'hi'
     })
     message_id = message_id.json()
-    messages = requests.get(f"{url}/dm/messages/v1", json={
+    messages = requests.get(f"{url}/dm/messages/v2", json={
         'token': reg_info1['token'],
         'dm_id': dm_2['dm_id'],
         'start': 0
     })
     messages = messages.json()
-    assert messages['end'] == -1
+    assert messages["end"] == -1
 
 def test_dm_messages_v1_many_messages(clear):
     """
@@ -576,9 +809,9 @@ def test_dm_messages_v1_many_messages(clear):
     dm_2 = dm_2.json()
     for i in range(0, 50):
         message_id = requests.post(f"{url}/message/senddm/v1", json={
-        'token': reg_info1['token'],
-        'dm_id': dm_2['dm_id']
-        'message': 'hi'
+            'token': reg_info1['token'],
+            'dm_id': dm_2['dm_id'],
+            'message': 'hi'
         })
         message_id = message_id.json()
     messages = requests.get(f"{url}/dm/messages/v1", json={
@@ -586,7 +819,6 @@ def test_dm_messages_v1_many_messages(clear):
         'dm_id': dm_2['dm_id'],
         'start': 0
     })
-    messages = messages.json()
-    assert messages['end'] == 50
-        
+    messages = messages.json()   
+    assert messages["end"] == 50
 
