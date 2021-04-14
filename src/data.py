ce7157ba09290f282fbd411badcfe011f5d7ece1
Gordon <z5310742@student.unsw.edu.au>
2021-04-15 00:37:50 +1000

    Changed back name

---
+++ src/data.py
@@ -0,0 +1,12 @@
+data = {
+    "users" : [],
+    "channels" : [],
+    "dms": [],
+    "message_ids" : [],
+    "notifications" : [],
+    "token_list": []
+}
+# with open('store.json', 'r') as FILE:
+#         data = loads(FILE.read())
+# with open('store.json', 'w') as FILE:
+#         FILE.write(dumps(data))
