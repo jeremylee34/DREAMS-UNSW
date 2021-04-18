a8b48447ca4daf2a2f49738615eb0ffcabda309f
z5255037 <z5255037@cse.unsw.edu.au>
2021-04-19 00:29:40 +1000

    Fixed up stat functions

--- src/user.py
+++ src/user.py
@@ -5,7 +5,7 @@ Written by Kanit Srihakorth and Tharushi Gunawardana
 '''
 from flask import request
 import jwt
-from data import data
+import data
 from error import InputError, AccessError
 import re
 from auth import auth_register_v1
@@ -36,14 +36,14 @@ def user_profile_v1(token, u_id):
     valid = 0
     valid_token = 0
     profile = {}
-    for t in data["token_list"]:
+    for t in data.data["token_list"]:
         if token == t:
             valid_token = 1
     #If token is valid, then profile dict is updated otherwise access error is thrown
     if valid_token == 1:
         decoded_token = jwt.decode(token, SECRET, algorithms=['HS256'])
         #Getting information for user profile
-        for x in data["users"]:
+        for x in data.data["users"]:
                 if u_id == x['u_id']:
                     valid = 1
                     for y in x["session_ids"]:
@@ -77,7 +77,7 @@ def user_profile_setname_v1(token, name_first, name_last):
         Empty dictionary
     """
     valid_token = 0
-    for t in data["token_list"]:
+    for t in data.data["token_list"]:
         if token == t:
             valid_token = 1    
     #If token is valid, then profile dict is updated otherwise access error is thrown
@@ -90,7 +90,7 @@ def user_profile_setname_v1(token, name_first, name_last):
         if len(name_last) < 1 or len(name_last) > 50:
             raise InputError("Invalid lastname")        
         #Updating user firstname and lastname
-        for x in data["users"]:
+        for x in data.data["users"]:
             for y in x["session_ids"]:
                 if decoded_token["session_id"] == y:
                     x['name_first'] = name_first
@@ -113,7 +113,7 @@ def user_profile_setemail_v1(token, email):
         Empty dictionary
     """
     valid_token = 0
-    for t in data["token_list"]:
+    for t in data.data["token_list"]:
         if token == t:
             valid_token = 1
     #If token is valid, then profile dict is updated otherwise access error is thrown
@@ -126,7 +126,7 @@ def user_profile_setemail_v1(token, email):
         else:    
             raise InputError("Invalid email")
         #checking for if email is already used
-        for x in data["users"]:
+        for x in data.data["users"]:
             if x["email"] == email:
                 raise InputError("Email is already used")
             else:
@@ -153,7 +153,7 @@ def user_profile_sethandle_v1(token, handle_str):
         Empty dictionary
     """  
     valid_token = 0
-    for t in data["token_list"]:
+    for t in data.data["token_list"]:
         if token == t:
             valid_token = 1
     #If token is valid, then profile dict is updated otherwise access error is thrown
@@ -162,7 +162,7 @@ def user_profile_sethandle_v1(token, handle_str):
         #Checks if handle_str is valid
         if len(handle_str) < 3 or len(handle_str) > 20:
             raise InputError("Invalid handle")
-        for x in data['users']:
+        for x in data.data['users']:
             #Checks if handle is already used
             if x['handle_str'] == handle_str:
                 raise InputError("Handle already used")
@@ -188,7 +188,7 @@ def users_all_v1(token):
         A list containig all of the users and their information
     """
     valid_token = 0
-    for t in data["token_list"]:
+    for t in data.data["token_list"]:
         if token == t:
             valid_token = 1
     #If token is valid, then profile dict is updated otherwise access error is thrown
@@ -196,7 +196,7 @@ def users_all_v1(token):
         all_users = []
         info = {}
         #Gets the user information
-        for x in data["users"]:
+        for x in data.data["users"]:
             info['u_id'] = x['u_id']
             info['email'] = x['email']
             info['name_first'] = x['name_first']
@@ -212,19 +212,19 @@ def users_all_v1(token):
 def user_stats_v1(token):
     valid = 0
     #Checking to see if token is valid
-    for t in data['token_list']:
+    for t in data.data['token_list']:
         if t == token:
             valid = 1
     #If token is valid then returns the user stats, otherwise raises input error
     if valid == 1:
         decoded_token = jwt.decode(token, SECRET, algorithms=['HS256'])
         #Finding the user
-        for i in data["users"]:
+        for i in data.data["users"]:
             for j in i["session_ids"]:
                 if decoded_token["session_id"] == j:
                     u_id = i['u_id']
         #Getting number of channels user is in
-        num_user_channels = data['users'][u_id]['num_channels']
+        num_user_channels = data.data['users'][u_id]['num_channels']
         #Getting total number of channels
         all_channels = channels_listall_v1(token)
         total_channels = len(all_channels['channels'])
@@ -232,15 +232,17 @@ def user_stats_v1(token):
         dms = dm.dm_list_v1(token)
         num_user_dms = len(dms['dms'])
         #Getting total number of dms
-        total_dms = len(data['dms'])
+        total_dms = len(data.data['dms'])
         #Getting number of messages user has sent
-        num_user_messages = data['users'][u_id]['num_messages']
+        num_user_messages = data.data['users'][u_id]['num_messages']
         #Getting total number of messages
-        total_messages = len(data["message_ids"])
+        total_messages = len(data.data["message_ids"])
         #Calculating involvement rate
         numerator = num_user_messages + num_user_dms + num_user_channels
         denominator = total_channels + total_dms + total_messages
-        involvement_rate = float(numerator / denominator)
+        involvement_rate = 0
+        if denominator != 0:
+            involvement_rate = float(numerator / denominator)
         #Getting the timestamp
         current_time = datetime.now()
         timestamp = current_time.replace(tzinfo=timezone.utc).timestamp()
@@ -248,47 +250,50 @@ def user_stats_v1(token):
         new_stat = {}
         count = 0
         i = 0
-        if len(data["user_stats"]) == 0:
+        num_stats = False
+        if len(data.data["user_stats"]) == 0:
             new_stat["channels_joined"] = [{'joined': num_user_channels, 'time': timestamp}]
             new_stat["dms_joined"] = [{'joined': num_user_dms, 'time': timestamp}]
             new_stat["messages_sent"] = [{'sent': num_user_messages, 'time': timestamp}]
             new_stat["u_id"] = u_id          
-            data["user_stats"].append(new_stat)
+            data.data["user_stats"].append(new_stat)
         else:   
-            for check in data['user_stats']:
+            for check in data.data['user_stats']:
                 if check['u_id'] == u_id:
                     count = i
+                    num_stats = True
                     if check['channels_joined'][len(check['channels_joined'])-1]['joined'] == num_user_channels:
                         channel_time = check['channels_joined'][len(check['channels_joined'])-1]['time']
                         new_stat = {'joined': num_user_channels, 'time': channel_time}
-                        data['user_stats'][count]['channels_joined'].append(new_stat)
+                        data.data['user_stats'][count]['channels_joined'].append(new_stat)
                     else:
                         new_stat = {'joined': num_user_channels, 'time': timestamp}
-                        data['user_stats'][count]['channels_joined'].append(new_stat)
+                        data.data['user_stats'][count]['channels_joined'].append(new_stat)
                     if check['dms_joined'][len(check['dms_joined'])-1]['joined'] == num_user_dms:
                         dm_time = check['dms_joined'][len(check['dms_joined'])-1]['time']
                         new_stat = {'joined': num_user_dms, 'time': dm_time}
-                        data['user_stats'][count]['dms_joined'].append(new_stat)
+                        data.data['user_stats'][count]['dms_joined'].append(new_stat)
                     else:
                         new_stat = {'joined': num_user_dms, 'time': timestamp}
-                        data['user_stats'][count]['dms_joined'].append(new_stat) 
+                        data.data['user_stats'][count]['dms_joined'].append(new_stat) 
                     if check['messages_sent'][len(check['messages_sent'])-1]['sent'] == num_user_messages:
                         message_time = check['messages_sent'][len(check['messages_sent'])-1]['time']
                         new_stat = {'sent': num_user_messages, 'time': message_time}
-                        data['user_stats'][count]['messages_sent'].append(new_stat)
+                        data.data['user_stats'][count]['messages_sent'].append(new_stat)
                     else:
                         new_stat = {'sent': num_user_messages, 'time': timestamp}
-                        data['user_stats'][count]['messages_sent'].append(new_stat)  
-                else:
-                    new_stat["channels_joined"] = [{'joined': num_user_channels, 'time': timestamp}]
-                    new_stat["dms_joined"] = [{'joined': num_user_dms, 'time': timestamp}]
-                    new_stat["messages_sent"] = [{'sent': num_user_messages, 'time': timestamp}]
-                    new_stat["u_id"] = u_id          
-                    data["user_stats"].append(new_stat)                    
+                        data.data['user_stats'][count]['messages_sent'].append(new_stat)                     
                 i += 1
+            if num_stats == False:
+                new_stat["channels_joined"] = [{'joined': num_user_channels, 'time': timestamp}]
+                new_stat["dms_joined"] = [{'joined': num_user_dms, 'time': timestamp}]
+                new_stat["messages_sent"] = [{'sent': num_user_messages, 'time': timestamp}]
+                new_stat["u_id"] = u_id          
+                data.data["user_stats"].append(new_stat) 
+                count = i
     else:
         raise InputError("Invalid token")
-    return_stat = data['user_stats'][count].copy()
+    return_stat = data.data['user_stats'][count].copy()
     remove_u_id = return_stat.pop('u_id')
     return_stat['involvement_rate'] = involvement_rate
     return return_stat
@@ -296,7 +301,7 @@ def user_stats_v1(token):
 def users_stats_v1(token):
     valid = 0
     #Checking to see if token is valid
-    for t in data['token_list']:
+    for t in data.data['token_list']:
         if t == token:
             valid = 1
     #If token is valid then returns the users stats, otherwise raises input error
@@ -306,22 +311,22 @@ def users_stats_v1(token):
         total_channels = len(all_channels['channels'])
         #Getting number of dms that currently exist
         existing_dms = 0
-        for x in data["dms"]:
+        for x in data.data["dms"]:
             if x["dm_id"] != '':
                 existing_dms += 1
         #Getting number of existing messages
         existing_messages = 0
-        for y in data["channels"]:
+        for y in data.data["channels"]:
             for z in y["messages"]:
                 if z["message"] != '':
                     existing_messages += 1
-        for a in data["dms"]:
+        for a in data.data["dms"]:
             for b in a["messages"]:
                 if b["message"] != '':
                     existing_messages += 1
         #Finding the users joined in at least one channel or dm
         users_joined = []
-        for i in data["channels"]:
+        for i in data.data["channels"]:
             for j in i["all_members"]:
                 if users_joined == []:
                     users_joined.append(j["u_id"])
@@ -333,7 +338,7 @@ def users_stats_v1(token):
                     if joined == 0:
                         users_joined.append(j["u_id"])
 
-        for i in data["dms"]:
+        for i in data.data["dms"]:
             for j in i["members"]:
                 if users_joined == []:
                     users_joined.append(j["u_id"])
@@ -344,9 +349,8 @@ def users_stats_v1(token):
                             joined = 1
                     if joined == 0:
                         users_joined.append(j["u_id"])
-        print(users_joined)
         #Calculating utilization_rate
-        utilization_rate = float(len(users_joined) / len(data["users"]))
+        utilization_rate = float(len(users_joined) / len(data.data["users"]))
         #Getting the timestamp
         current_time = datetime.now()
         timestamp = current_time.replace(tzinfo=timezone.utc).timestamp()
@@ -354,54 +358,55 @@ def users_stats_v1(token):
         new_stat = {}
         count = 0
         i = 0
-        if len(data["dreams_stats"]) == 0:
+        if len(data.data["dreams_stats"]) == 0:
             new_stat["channels_exist"] = [{'exist': total_channels, 'time': timestamp}]
             new_stat["dms_exist"] = [{'exist': existing_dms, 'time': timestamp}]
             new_stat["messages_exist"] = [{'exist': existing_messages, 'time': timestamp}]        
-            data["dreams_stats"] = new_stat
+            data.data["dreams_stats"] = new_stat
         else:
-            check = data['dreams_stats']
+            check = data.data['dreams_stats']
             if check['channels_exist'][len(check['channels_exist'])-1]['exist'] == total_channels:
                 channel_time = check['channels_exist'][len(check['channels_exist'])-1]['time']
                 new_stat = {'exist': total_channels, 'time': channel_time}
-                data['dreams_stats']['channels_exist'].append(new_stat)
+                data.data['dreams_stats']['channels_exist'].append(new_stat)
             else:
                 new_stat = {'exist': total_channels, 'time': timestamp}
-                data['dreams_stats']['channels_exist'].append(new_stat)
+                data.data['dreams_stats']['channels_exist'].append(new_stat)
             if check['dms_exist'][len(check['dms_exist'])-1]['exist'] == existing_dms:
                 dm_time = check['dms_exist'][len(check['dms_exist'])-1]['time']
                 new_stat = {'exist': existing_dms, 'time': dm_time}
-                data['dreams_stats']['dms_exist'].append(new_stat)
+                data.data['dreams_stats']['dms_exist'].append(new_stat)
             else:
                 new_stat = {'exist': existing_dms, 'time': timestamp}
-                data['dreams_stats']['dms_exist'].append(new_stat) 
+                data.data['dreams_stats']['dms_exist'].append(new_stat) 
             if check['messages_exist'][len(check['messages_exist'])-1]['exist'] == existing_messages:
                 message_time = check['messages_exist'][len(check['messages_exist'])-1]['time']
                 new_stat = {'exist': existing_messages, 'time': message_time}
-                data['dreams_stats']['messages_exist'].append(new_stat)
+                data.data['dreams_stats']['messages_exist'].append(new_stat)
             else:
                 new_stat = {'exist': num_user_dms, 'time': timestamp}
-                data['dreams_stats']['messages_exist'].append(new_stat)
+                data.data['dreams_stats']['messages_exist'].append(new_stat)
     else:
         raise InputError("Invalid token")
-    data['dreams_stats']['utilization_rate'] = utilization_rate
-    return data['dreams_stats']
+    data.data['dreams_stats']['utilization_rate'] = utilization_rate
+    return data.data['dreams_stats']
 
 import re
 def user_profile_uploadphoto_v1(token, img_url, x_start, y_start, x_end, y_end):
     valid = 0
     #Checking to see if token is valid
-    for t in data['token_list']:
+    for t in data.data['token_list']:
         if t == token:
             valid = 1
     #If token is valid then returns the users stats, otherwise raises input error
     if valid == 1:   
         decoded_token = jwt.decode(token, SECRET, algorithms=['HS256'])
         u_id = ''
-        for x in data["users"]:
+        for x in data.data["users"]:
             for y in x["session_ids"]:
                 if decoded_token["session_id"] == y:
                     u_id = x['u_id']  
+        #Checking if img_url ends with jpg
         regex = '.*jpg$'
         if re.search(regex, img_url):
             pass
@@ -410,23 +415,23 @@ def user_profile_uploadphoto_v1(token, img_url, x_start, y_start, x_end, y_end):
         #Downloaded the photo
         fullfilename = os.path.join("static", f"user{u_id}_photo.jpg")
         urllib.request.urlretrieve(img_url, fullfilename)
-        #Crop image
         image = Image.open(fullfilename)  
         width, height = image.size
-        print(width, height)
+        #Accounting for invalid x inputs
         if x_start > width or x_end > width or x_start < 0 or x_end < 0:
             raise InputError("Invalid x dimensions")
-        print(y_start, height, y_end, height)
         if x_start == x_end:
             raise InputError("Invalid x dimensions")
+        #Accounting for invalid y inputs
         if y_start > height or y_end > height or y_start < 0 or y_end < 0:
             raise InputError("Invalid y dimensions")
         if y_start == y_end:
             raise InputError("Invalid y dimensions")
+        #Cropping image
         crop_image = image.crop((x_start,y_start,x_end,y_end))
         crop_image.save(fullfilename)
         #Adding link to data
-        data["users"][u_id]["img_url"] = f'http://127.0.0.1:{port}/' + fullfilename
+        data.data["users"][u_id]["img_url"] = f'http://127.0.0.1:{port}/' + fullfilename
     else:
         raise InputError("Invalid token")
     return {}
@@ -454,14 +459,16 @@ if __name__ == '__main__':
     # message_remove_v1(r['token'], 2)
     # message_remove_v1(r['token'], 1)
     # message_remove_v1(r['token'], 0)
-    # p = user_stats_v1(r['token'])
-    # print(p)
-    # t = user_stats_v1(s['token'])
-    # print(t)
-    # o = user_stats_v1(k['token'])
-    # print(o)
-    # e = users_stats_v1(r['token'])
-    # print(e)
+    p = user_stats_v1(r['token'])
+    print(p)
+    dms3 = dm.dm_create_v1(r['token'], [1])
+    p = user_stats_v1(r['token'])
+    print(p)
+    t = user_stats_v1(s['token'])
+    print(t)
+    o = user_stats_v1(k['token'])
+    print(o)
+    e = users_stats_v1(r['token'])
     user_profile_uploadphoto_v1(r['token'], IMG_URL, 0, 0, 1500, 1000)  
     # y = user_stats_v1(s['token'])
     # print(y)
